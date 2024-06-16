838a9ca1805530b607c6c94ce1fa4b6afbf3ebf9
TyreseJerome <tyresejeromeogtj@gmail.com>
2024-06-16 02:48:00 +0100

    Update index.blade.php

--- resources/views/Tickets/index.blade.php
+++ resources/views/Tickets/index.blade.php
@@ -7,15 +7,16 @@
     <div class="flex justify-center">
         <div class="my-4 p-6 bg-white dark:bg-gray-900 overflow-hidden
                     shadow-sm sm:rounded-lg text-gray-900 dark:text-gray-50">
-            {{-- Asumindo que você pode ter filtros para tickets também, por exemplo, por filme ou data --}}
-            <x-tickets.filter-card
-                :filterAction="route('tickets.index')"
-                :resetUrl="route('tickets.index')"
-                :movies="$movies->pluck('title', 'id')->toArray()" {{-- Substituindo cursos por filmes --}}
-                :movie="old('movie', $filterByMovie)"
-                :date="old('date', $filterByDate)"
-                class="mb-6"
-                />
+            @if(!$purchase)
+                <x-tickets.filter-card
+                    :filterAction="route('tickets.index')"
+                    :resetUrl="route('tickets.index')"
+                    :movies="$movies?->toArray()"
+                    :movie="old('movie', $filterByMovie ?? null)"
+                    :date="old('date', $filterByDate ?? null)"
+                    class="mb-6"
+                    />
+            @endif
             @can('create', App\Models\Ticket::class)
                 <div class="flex items-center gap-4 mb-4">
                     <x-button
@@ -27,15 +28,19 @@
             <div class="font-base text-sm text-gray-700 dark:text-gray-300">
             <x-tickets.table :tickets="$tickets"
                 :showView="true"
-                :showEdit="true"
-                :showDelete="true"
-                :showAddToCart="true"
-                :showRemoveFromCart="true"
-                />
+                :showEdit="Auth::check() && Auth::user()->type != 'C'"
+                :showDelete="Auth::check() && Auth::user()->type != 'C'"
+                :showAddToCart="false"
+                :showRemoveFromCart="false"
+            />
             </div>
-            <div class="mt-4">
-                {{ $tickets->links() }} {{-- Paginação dos tickets --}}
-            </div>
+            @if($purchase)
+                <x-button class="mt-10" href="{{ route('purchases.show', $purchase) }}" type="primary" text="Return to purchase"/>
+            @else
+                <div class="mt-4">
+                    {{ $tickets->links() }}
+                </div>
+            @endif
         </div>
     </div>
 @endsection
