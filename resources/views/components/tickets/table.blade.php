5cd235049cedf3215c9f85d8e419f4f8b2b37a54
TyreseJerome <tyresejeromeogtj@gmail.com>
2024-06-16 02:40:42 +0100

    Update table.blade.php

--- resources/views/components/tickets/table.blade.php
+++ resources/views/components/tickets/table.blade.php
@@ -6,7 +6,7 @@
             <th class="px-2 py-2 text-left">Date</th>
             <th class="px-2 py-2 text-left">Time</th>
             <th class="px-2 py-2 text-left">Theater</th>
-            <th class="px-2 py-2 text-left">Seat Number</th>
+            <th class="px-2 py-2 text-left">Seat</th>
             <th class="px-2 py-2 text-left">Price</th>
             <th class="px-2 py-2 text-left">Status</th>
             @if($showView)
@@ -28,44 +28,41 @@
         </thead>
         <tbody>
         @foreach ($tickets as $ticket)
+            @php 
+                $isTempTicket = ! \App\Models\Ticket::where('id', $ticket->id)->exists(); 
+            @endphp
             <tr class="border-b border-b-gray-400 dark:border-b-gray-500">
                 <td class="px-2 py-2 text-left">{{ $ticket->screening->movie->title }}</td>
                 <td class="px-2 py-2 text-left">{{ $ticket->screening->date }}</td>
                 <td class="px-2 py-2 text-left">{{ $ticket->screening->start_time }}</td>
                 <td class="px-2 py-2 text-left">{{ $ticket->screening->theater->name }}</td>
-                <td class="px-2 py-2 text-center">{{ $ticket->seat->seat_number }}</td>
+                <td class="px-2 py-2 text-center">{{ $ticket->seat->row . $ticket->seat->seat_number }}</td>
                 <td class="px-2 py-2 text-left">{{ $ticket->price }}</td>
                 <td class="px-2 py-2 text-left">{{ $ticket->status }}</td>
                 @if($showView)
                     <td>
                         <x-table.icon-show class="ps-3 px-0.5"
-                            href="{{ route('tickets.show', ['ticket' => $ticket->id]) }}"/>
+                            href="{{ $isTempTicket ? route('tickets.showBySession', ['screeningId' => $ticket->screening_id, 'seatId' => $ticket->seat_id]) :
+                                                     route('tickets.show', ['ticket' => $ticket])}}"/>
                     </td>
                 @endif
-                @if($showEdit)
+                @if($showEdit && !$isTempTicket)
                     <td>
                         <x-table.icon-edit class="px-0.5"
                             href="{{ route('tickets.edit', ['ticket' => $ticket->id]) }}"/>
                     </td>
                 @endif
-                @if($showDelete)
+                @if($showDelete && !$isTempTicket)
                     <td>
                         <x-table.icon-delete class="px-0.5"
                             action="{{ route('tickets.destroy', ['ticket' => $ticket->id]) }}"/>
-                    </td>
-                @endif
-                @if($showAddToCart)
-                    <td>
-                        <x-table.icon-add-cart class="px-0.5"
-                            method="post"
-                            action="{{ route('cart.add', ['ticket' => $ticket->id]) }}"/>
                     </td>
                 @endif
                 @if($showRemoveFromCart)
                     <td>
                         <x-table.icon-minus class="px-0.5"
                             method="delete"
-                            action="{{ route('cart.remove', ['ticket' => $ticket->id]) }}"/>
+                            action="{{ route('cart.remove', ['screeningId' => $ticket->screening_id, 'seatId' => $ticket->seat_id]) }}"/>
                     </td>
                 @endif
             </tr>
