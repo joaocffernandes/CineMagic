2be428137139067716f42654f446d53f1f337ab7
TyreseJerome <tyresejeromeogtj@gmail.com>
2024-06-16 02:27:27 +0100

    Update TicketController.php

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,44 +4,98 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+use App\Mail\PurchaseReceipt;
 use App\Http\Requests\DisciplineFormRequest;
 use App\Models\Ticket;
 use App\Models\Movie;
+use App\Models\Purchase;
+use App\Services\QrCodeService;
 use Illuminate\View\View;
+use PDF; // dompdf
+use Mpdf\Mpdf; // mpdf
+
+use App\Models\Screening;
 
 class TicketController extends Controller
 {
-    public function index(Request $request): View
+    public function index(Request $request)
     {
-        $filterByMovie = $request->query('movie');
-        $filterByDate = $request->query('date');
+        $ticketsQuery = Ticket::with(['screening.movie', 'screening.theater', 'seat', 'purchase']);
+                            //->whereHas('screening', function ($query) { $query->where('date', '>=', now()->format('Y-m-d'));  })
+
+        if (auth()->user()->type != 'A')  
+        {
+            $ticketsQuery->whereHas('purchase', function ($query) {
+                $customerId = auth()->user()->customer->id;  // Garante que está autenticado e tem um cliente associado
+                $query->where('customer_id', $customerId);
+            });
+        }
+                            
+        $ticketsQuery->orderBy('created_at', 'desc');
+
         
-        $ticketsQuery = Ticket::query()->with('screening.movie');
-
-        if ($filterByMovie !== null) {
-            $ticketsQuery->whereHas('screening.movie', function ($query) use ($filterByMovie) {
-                $query->where('id', $filterByMovie);
+        $movieIds = $ticketsQuery->get()->pluck('screening.movie_id')->unique()->filter();
+        $movies = Movie::whereIn('id', $movieIds)->orderBy('title', 'asc')->pluck('title', 'id');
+        
+    
+        if ($request->filled('purchase')) {
+            $purchaseId = $request->purchase;
+            $ticketsQuery->whereHas('purchase', function ($query) use ($purchaseId) {
+                $query->where('id', $purchaseId);
             });
+        } else {
+            if ($request->filled('movie')) {
+                $ticketsQuery->whereHas('screening.movie', function ($query) use ($request) {
+                    $query->where('id', $request->movie);
+                });
+            }
+        
+            if ($request->filled('date')) {
+                $ticketsQuery->whereHas('screening', function ($query) use ($request) {
+                    $query->where('date', $request->date);
+                });
+            }
         }
-        if ($filterByDate !== null) {
-            $ticketsQuery->whereHas('screening', function ($query) use ($filterByDate) {
-                $query->where('date', $filterByDate);
-            });
-        }
-
-        $tickets = $ticketsQuery->whereHas('screening')->paginate(20)->withQueryString();
-        $movies = Movie::pluck('title', 'id');
-
-        return view('tickets.index', compact('tickets', 'movies', 'filterByMovie', 'filterByDate'));
+    
+        $tickets = $ticketsQuery->paginate(20)->withQueryString();
+        $purchase = $request->purchase ?? null;
+            
+        return view('tickets.index', compact('tickets', 'movies', 'purchase'));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Ticket $ticket): View
+    public function showByTicket(Ticket $ticket)
     {
-        return view('tickets.show')
-            ->with('ticket', $ticket);
+        if ($ticket->purchase->customer) 
+        {
+            if (auth()->id() === $ticket->purchase->customer->id || auth()->user()->type == 'A') 
+            {
+                return view('tickets.show')
+                    ->with('ticket', $ticket);
+            }
+        }
+
+        return back()->with('alert-msg', 'You do not have permission to view this ticket.')->with('alert-type', 'danger');
+    }
+
+    public function showBySession(int $screeningId, int $seatId)
+    {
+        $tickets = session('cart', collect());
+    
+        $ticketIndex = $tickets->search(function ($ticket) use ($screeningId, $seatId) {
+            return $ticket->screening_id == $screeningId && $ticket->seat_id == $seatId;
+        });
+    
+        if ($ticketIndex !== false) {
+            $ticket = $tickets->get($ticketIndex);
+            
+            return view('tickets.show', compact('ticket'));
+        }
+    
+        return back()->with('alert-msg', 'Ticket do not exist.')->with('alert-type', 'warning');
     }
 
     /**
@@ -90,5 +144,34 @@ class TicketController extends Controller
         return redirect()->route('tickets.index')
             ->with('alert-type', $alertType)
             ->with('alert-msg', $alertMsg);
+    }
+
+    public function downloadReceipt(Ticket $ticket)
+    {
+        if (auth()->id() === $ticket->purchase->customer->id || auth()->user()->type == 'A') 
+        {
+            $qrImageSrc = QrCodeService::getQrCodeImageBase64($ticket->qrcode_url);
+        
+            $html = view('emails.tickets.receipt', compact('ticket', 'qrImageSrc'))->render();
+        
+            $mpdf = new Mpdf();
+            $mpdf->WriteHTML($html);
+            $fileName = 'Receipt-' . $ticket->id . '.pdf';
+            $mpdf->Output($fileName, 'D'); // 'D' for download
+            return back();
+        }
+
+        return back()->with('alert-msg', 'You do not have permission to donwload this ticket or it does not exist.')->with('alert-type', 'danger');
+    }
+    
+    public function showReceipt(Ticket $ticket) // For test
+    {    
+        if (auth()->id() === $ticket->purchase->customer->id || auth()->user()->type == 'A') 
+        {
+            $qrImageSrc = QrCodeService::getQrCodeImageBase64($ticket->qrcode_url);
+            return view('emails.tickets.receipt', compact('ticket', 'qrImageSrc'));
+        }
+
+        return back()->with('alert-msg', 'You do not have permission to view this ticket or it does not exist.')->with('alert-type', 'danger');
     }
 }
